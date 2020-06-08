92faaf39d216b61746b2d833f3f5580704d728d9
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-08 15:50:09 +0530

    Fix markdown

--- stdplugins/reverse.py
+++ stdplugins/reverse.py
@@ -10,11 +10,11 @@ async def _(event):
     tracemoe = tracemoepy.tracemoe.TraceMoe()
     search = tracemoe.search(file, encode=True).json()
     result = search['docs'][0]
-    msg = f"*Title*: {result['title_english']}"\
-          f"\n*Similarity*: {str(result['similarity'])[0:2]}"\
-          f"\n*Episode*: {result['episode']}"
+    msg = f"**Title**: {result['title_english']}"\
+          f"\n**Similarity**: {str(result['similarity'])}"\
+          f"\n**Episode**: {result['episode']}"
     preview = tracemoe.video_preview(search)
     with open('preview.mp4', 'wb') as f:
      f.write(preview)
     await event.reply(msg)
-    await borg.send_file(preview, caption = msg, force_document=False)
+    await borg.send_file(event.chat_id, preview, caption = msg, force_document=False)
