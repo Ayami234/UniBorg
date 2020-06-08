6e608df0aa04afde80445417012ee7453bb9daf2
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-08 15:29:35 +0530

    will push better fix soon

--- stdplugins/reverse.py
+++ stdplugins/reverse.py
@@ -8,10 +8,11 @@ async def _(event):
     reply_message = await event.get_reply_message()
     file = await borg.download_media(reply_message)
     tracemoe = tracemoepy.tracemoe.TraceMoe()
-    search = tracemoe.search(file, encode=True)
+    search = tracemoe.search(file, encode=True).json()
     result = search['docs'][0]
     msg = (f"{result['title_english']}\n{result['similarity'][0:2]}")
     preview = tracemoe.video_preview(search)
     with open('preview.mp4', 'wb') as f:
      f.write(preview)
+    await event.reply(msg)
     await borg.send_document(preview, caption = msg, force_document=False)
