7e4631f0c86d09a63727688ec8a64eedc44a55f9
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-08 15:45:22 +0530

    Better reply

--- stdplugins/reverse.py
+++ stdplugins/reverse.py
@@ -1,7 +1,7 @@
 from uniborg.util import admin_cmd
 import tracemoepy
 
-@borg.on(admin_cmd("reverse "))
+@borg.on(admin_cmd("reverse"))
 async def _(event):
     if event.fwd_from:
         return
@@ -10,9 +10,11 @@ async def _(event):
     tracemoe = tracemoepy.tracemoe.TraceMoe()
     search = tracemoe.search(file, encode=True).json()
     result = search['docs'][0]
-    msg = (f"{result['title_english']}\n{str(result['similarity'])[0:2]}")
+    msg = f"*Title*: {result['title_english']}"\
+          f"\n*Similarity*: {str(result['similarity'])[0:2]}"\
+          f"\n*Episode*: {result['episode']}"
     preview = tracemoe.video_preview(search)
     with open('preview.mp4', 'wb') as f:
      f.write(preview)
     await event.reply(msg)
-    await borg.send_document(preview, caption = msg, force_document=False)
+    await borg.send_file(preview, caption = msg, force_document=False)
