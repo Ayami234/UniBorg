b4beffc758bee7a1bed4dab241da1fbdf0046023
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-09 13:57:44 +0530

    Update reverse.py

--- stdplugins/reverse.py
+++ stdplugins/reverse.py
@@ -7,14 +7,14 @@ async def _(event):
         return
     reply_message = await event.get_reply_message()
     file = await borg.download_media(reply_message)
-    tracemoe = tracemoepy.tracemoe.TraceMoe()
-    search = tracemoe.search(file, encode=True).json()
+    tracemoe = tracemoepy.async_trace.Async_Trace()
+    search = await tracemoe.search(file, encode=True).json()
     result = search['docs'][0]
     msg = f"**Title**: {result['title_english']}"\
-          f"\n**Similarity**: {str(result['similarity'])}"\
+          f"\n**Similarity**: {str(result['similarity'])[1:2]}"\
           f"\n**Episode**: {result['episode']}"
-    preview = tracemoe.video_preview(search)
+    preview = await tracemoe.video_preview(search)
     with open('preview.mp4', 'wb') as f:
      f.write(preview)
-    await event.reply(msg)
-    await borg.send_file(event.chat_id, 'preview.mp4', caption = msg, force_document=False)
+    await event.edit(msg)
+    await borg.send_file(event.chat_id, 'preview.mp4', caption = 'Match', force_document=False)
